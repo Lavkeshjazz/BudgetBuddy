69d7878996ec8c55b1e8b625ddc341168b531f03
Lavkeshjazz <lavkesh.jaiswal34@gmail.com>
2024-04-01 23:15:18 +0530

    lavkesh:new urls and expectedprice now can be added in the exisiting database

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,17 @@
 const express = require("express");
 var bodyParser = require('body-parser');
 const axios = require("axios");
-const { defaultPage, searchResult,checkforemail,resetPassword,forgotPassword,renderResetPassword, addUrlinDatabase} = require("../controller/user");
+//const { defaultPage, searchResult,checkforemail,resetPassword,forgotPassword,renderResetPassword, addUrlinDatabase} = require("../controller/user");
 
 
-const { defaultPage, searchResult,checkforemail,resetPassword,forgotPassword,renderResetPassword, addUrlinDatabase,deleteDatabase} = require("../controller/user");
+const { defaultPage, searchResult,checkforemail,resetPassword,forgotPassword,renderResetPassword, addUrlinDatabase,deleteDatabase,add_new_data_in_existing_database} = require("../controller/user");
 const { restrictToSearchRoute, checkAuth, restrictToLoggedinUserOnly} = require("../middlewares/auth");
 const router = express.Router();
 router.use(bodyParser.urlencoded({ extended: true }));
 router.get("/", defaultPage);
 router.post("/search", restrictToLoggedinUserOnly,addUrlinDatabase,searchResult);
 router.post("/delete", deleteDatabase);
+router.post("/add", add_new_data_in_existing_database);
 
 router.get("/search",(req,res)=>{
     return res.render("searchpage");
