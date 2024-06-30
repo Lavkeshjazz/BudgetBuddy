814bee67a02b0e3a082d0ad64e07627aa01d47a6
Lavkeshjazz <lavkesh.jaiswal34@gmail.com>
2024-07-01 00:20:37 +0530

    Signup frontend error handled

--- frontendreact/src/components/Signup.jsx
+++ frontendreact/src/components/Signup.jsx
@@ -34,123 +34,26 @@ const Signup = () => {
     //here value is it's value
     name = e.target.name;
     value = e.target.value;
-    
-    if(name === 'firstName' || name==='lastname'){
-      const regex = /^[a-zA-Z]*$/;
-      if(!regex.test(value)){
-        setErrors({...errors,[name]: "*Only Alphabetical letters are allowed."})
-      }
-      else{
-        setErrors({...errors, [name]: ''})
-      }
-    }
-    else if(name==='phone_number'){
-      const regex=/^[0-9]*$/;
-      if(!regex.test(value)){
-        setErrors({...errors,phone_number: "*Only Numberic digits are allowed."})
-      }
-      if(value.length !== 10){
-        setErrors({...errors,phone_number: "*The length of Phone number should only be 10"})
-      }
-      else{
-        setErrors({...errors,phone_number: ''});
-      }
-    }
-    else if(name==='email'){
-      const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      if(!regex.test(value)){
-        setErrors({...errors,email: "*Invalid Email Format"})
-      }
-      else{
-        setErrors({...errors,email:''})
-      }
-    }
-    else if(name==='password'){
-      if(value.length<8){
-        setErrors({...errors,password:"*Too Weak Level Password"})
-      }
-      else if(value.length>=8 && value.length<13){
-        setErrors({...errors,password:"*Medium Level Password"})
-      }
-      else{
-        setErrors({...errors,password:''});
-      }
-    }
-    else if (name === 'userType') {
-      if (value === '') {
-        setErrors({...errors, userType: "*Please select a user type."});
-      } else {
-        setErrors({...errors, userType: ''});
-      }
-    }
     setUser({ ...user, [name]: value });
   };
-  
-
-  
-
-  const redirectToOtpPage = async (e) => {
-    setOpen(true);
-    const { userType, phone_number,email } = user;
-    if (userType === 'trader') {
-      e.preventDefault();
-      try{
-      const response = await fetch("http://localhost:5000/trader/mail", {
-        method: 'POST',
-        body: JSON.stringify({ phone_number,email }),
-        headers: { 'Content-Type': 'application/json' },
-        credentials: 'include',
-      });
-      const responseData = await response.json(); // Parse response as JSON
-      if (response.ok) {
-        setOpen(false);
-        Swal.fire({
-          title: "Good job!",
-          text: "OTP Sent to Your Email for Two Step Verification",
-          icon: "success",
-          confirmButtonText: "Proceed",
-        }).then(() => {
-          history('/otpVerify', { state: user });
-        });
-      }
-      else {
-        setOpen(false);
-        Swal.fire({
-          icon: "error",
-          title: responseData.error.code || "Error",
-          text: responseData.error.message || "Please fill the details correctly.",
-          confirmButtonText: "Try Again",
-        });
-      }
-    } catch (error) {
-    setOpen(false);
-    console.error("Error fetching data:", error);
-    Swal.fire({
-        icon: "error",
-        title: "Network Error",
-        text: "An error occurred while communicating with the server.",
-        confirmButtonText: "OK",
+  const PostData = async (e) => {
+    e.preventDefault();
+    const { firstName, lastname, phone_number, email, password, userType } = user;
+    console.log("hello from postdata");
+    const res = await fetch('http://localhost:5000/user/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        firstName,
+        lastName: lastname,
+        phone_number,
+        email,
+        password,
+        userType
+      })
     });
-}
-}
-    else {
-      e.preventDefault();
-      const { firstName, lastname, phone_number, email, password, userType } = user;
-      console.log("hello from postdata");
-      const res = await fetch('http://localhost:5000/user/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          firstName,
-          lastName: lastname,
-          phone_number,
-          email,
-          password,
-          userType
-        })
-      });
     if (res.ok) {
       Swal.fire({
         title: "Good job!",
@@ -163,22 +66,10 @@ const Signup = () => {
     } else if (res.status === 400) {
       setOpen(false);
       const data = await res.json();
-      Swal.fire({
-        icon: "error",
-        title: data.error.code,
-        text: data.error.message,
-        confirmButtonText: "Try Again",
-      });
-    }
+      console.log(data);
+      window.alert(data.error.message);
     }
   };
-
-  // Check if there are any errors
-  const isDisabled = Object.values(errors).some(error => error !== '') || Object.values(user).some(value => value === '');
-
-  // Enable button only if there are no errors and userType is selected
-  const buttonDisabled = isDisabled || user.userType === '';
-
   return (
     <div className='signup'>
       <Navbar1 name="signin" id="loginbtn"/>
@@ -269,13 +160,8 @@ const Signup = () => {
                 id='userType'
                 value={user.userType}
                 onChange={handleInputs}
-                placeholder='User Type:'
-              >
-                <option value=''>Select User Type</option>
-                <option value='user'>User</option>
-                <option value='trader'>Trader</option>
-              </select>
-              {errors.userType && <span className='error'>{errors.userType}</span>}
+                placeholder='User / Trader'
+              />
             </div>
 
             <div className='form-group form-button'>
@@ -288,8 +174,7 @@ const Signup = () => {
                 id='signup'
                 className='signinbtn'
                 value='REGISTER'
-                onClick={redirectToOtpPage}
-                disabled={buttonDisabled}
+                onClick={PostData}
               />
             </div>
           </form>
